0e77cf5e7f33cd187b3e68c3de54b31bacbc4f78
LucasDiScipio <lucasdiscipio@hotmail.com>
2021-10-12 13:27:41 +0200

    premier arbre

--- tasks.py
+++ tasks.py
@@ -4,9 +4,14 @@ from sklearn.tree import DecisionTreeClassifier
 from utils import DT_to_PNG
 
 # import des donnees
-data = load_breast_cancer(as_frame=True)
+breast_cancer = load_breast_cancer()
+x = breast_cancer.data
+y = breast_cancer.target
+
 
 # separation des donnees
-training_data, testing_data = train_test_split(data.data, test_size=0.33, random_state=42)
-print(training_data)
-print(testing_data)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+
+# task 1
+clf = DecisionTreeClassifier(max_leaf_nodes=2)
+clf.fit(x_train, y_train)
