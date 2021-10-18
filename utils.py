90833acc908dca27344be89f7cf42fb35a0a0e7c
LucasDiScipio <lucasdiscipio@hotmail.com>
2021-10-18 11:13:52 +0200

    ajout de la fonction d'affichage format pdf

--- utils.py
+++ utils.py
@@ -28,3 +28,25 @@ def DT_to_PNG(model, feature_names, file_name):
     graph = pydot.graph_from_dot_data(dot_data.getvalue())[0]
     graph.write_png("%s.png" % file_name)
 
+
+def DT_to_PDF(model, feature_names, file_name):
+    """ Exports a DT to a PDF file for inspection.
+    
+    Parameters
+    ----------
+        - model: a decision tree (class sklearn.tree.DecisionTreeClassifier)
+        - feature_names: a list of feature names
+        - file_name: name of file to be produced (without '.png' extension)
+    
+    Notes
+    -----
+    This function requires the pydot Python package and the Graphviz library.
+    
+    For more information about tree export, see http://scikit-learn.org/stable/
+    modules/generated/sklearn.tree.export_graphviz.html#sklearn.tree.export_graphviz
+    """
+
+    dot_data = StringIO()
+    tree.export_graphviz(model, out_file=dot_data, feature_names=feature_names)
+    graph = pydot.graph_from_dot_data(dot_data.getvalue())[0]
+    graph.write_pdf("%s.pdf" % file_name)
